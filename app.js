c2146bbe7a1393c2cdcd1bcdd90e387d20ebb2ce
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2022-05-26 19:36:21 +0300

    express app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dbClient = require('./utils/db.js');
 const { resolveToken } = require('./utils/tokenMiddleware.js');
 
 const users = require('./controllers/users.js');
-const posts = require('./controllers/posts.js');
+const todo = require('./controllers/todo.js');
 
 const app = express();
 const { Router } = express;
@@ -13,7 +13,7 @@ app.use(express.json());
 async function run() {
   const db = await dbClient();
   app.use('/users', users(new Router(), db));
-  app.use('/posts', resolveToken(db), posts(new Router(), db));
+  app.use('/todo', resolveToken(db), todo(new Router(), db));
 }
 
 run();
